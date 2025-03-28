ca6b6a7f0d174de817af08187a090c889d215cdb
Franka <franka.bause@univie.ac.at>
2025-03-28 10:57:03 +0100

    more changes

--- poormans_page_generator.py
+++ poormans_page_generator.py
@@ -25,42 +25,8 @@ def create_homepage(pubtypes):
 	for pubtype in pubtypes:
 		Path(f'{pubtype}.html_part').unlink()
 
-
-def create_latex(pubtypes, baseurl, template):
-	# convert bibfile to tex lists
-	convert('franka.bib', pubtypes, format='tex')
-
-	# load files
-	index = Path(f'{template}.tex_template').read_text()
-
-	for pubtype in pubtypes:
-		reflist = Path(f'{pubtype}.tex_part').read_text()
-		index = index.replace(f'\\input{{{pubtype}.tex_part}}', reflist)
-
-	# add baseurl to relative urls for a standalone pdf version
-	index = re.sub('\\\\href\{(?!http)', f'\\\\href{{{baseurl}', index)
-
-	# write 
-	Path(f'{template}.tex').write_text(index)
-
-def compile_latex(template, pubtypes):
-	cmd = ['latexmk', '-xelatex', '-shell-escape', f'{template}.tex']
-	subprocess.run(cmd)
-
-	# clean up
-	for pubtype in pubtypes:
-		Path(f'{pubtype}.tex_part').unlink()
-
-	for extension in ['aux', 'fdb_latexmk', 'fls', 'log', 'out', 'xdv']:
-		Path(f'reflist.{extension}').unlink()
-
-
-
 if __name__ == '__main__':
 	# specify the categories
 	pubtypes = ['preprint', 'publication', 'lecturenote', 'nonarchival', 'book']
 
 	create_homepage(pubtypes)
-	create_latex(pubtypes, baseurl='https://frareba.github.io/', template='reflist')
-	create_latex(pubtypes + ['underreview'], baseurl='https://frareba.github.io/', template='reflistcv')
-	compile_latex('reflist', pubtypes + ['underreview'])
